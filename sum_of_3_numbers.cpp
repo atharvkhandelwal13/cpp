82eca6389ad92908d2f109100fe0709ff4b4abd2
atharvkhandelwal13 <atharvkhandelwal1307@gmail.com>
2026-03-16 19:44:28 +0530

    Implement sum of three numbers in C++

---
+++ sum_of_3_numbers.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c; 
+    cout<<"Enter 3 values: ";
+    cin>>a>>b>>c;
+    
+    cout<<"The sum of the three values is: "<<a+b+c;
+    cout<<"\n";
+    return 0;
+}
